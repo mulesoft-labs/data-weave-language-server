ecb0cafc6704f751a253c73af7f2b7c8e0564f40
martincousido <martincousido@gmail.com>
2021-06-30 16:53:39 -0300

    fix/DW-598 Keeping state while going back on wizard. (#21)
    * Fixing back and forth state for project creation
    
    * Fixing back button on file chooser

--- data-weave-lang-server/src/main/scala/org/mule/weave/lsp/ui/wizard/WidgetBuilder.scala
+++ data-weave-lang-server/src/main/scala/org/mule/weave/lsp/ui/wizard/WidgetBuilder.scala
@@ -2,25 +2,27 @@ package org.mule.weave.lsp.ui.wizard
 
 import org.mule.weave.lsp.ui.utils.Buttons.Button
 
-trait WidgetBuilder[A,T] {
+trait WidgetBuilder[A, T] {
 
-  def title(title: String) : WidgetBuilder[A,T]
+  def title(title: String): WidgetBuilder[A, T]
 
-  def stepNumber(number: Int) : WidgetBuilder[A,T]
+  def stepNumber(number: Int): WidgetBuilder[A, T]
 
-  def totalSteps(number: Int) : WidgetBuilder[A,T]
+  def totalSteps(number: Int): WidgetBuilder[A, T]
 
-  def button(button: Button, function: (T => WidgetResult[T])) : WidgetBuilder[A,T]
+  def button(button: Button, function: (T => WidgetResult[T])): WidgetBuilder[A, T]
 
-  def result( function: (T, A) => T):  WidgetBuilder[A,T]
+  def result(function: (T, A) => T): WidgetBuilder[A, T]
+
+  def selectionProvider(function: (T) => Option[A]): WidgetBuilder[A, T]
 
   def create(): Widget[T]
 
 }
 
-trait Widget[T]{
+trait Widget[T] {
 
-  def show(resultObject: T) : WidgetResult[T]
+  def show(resultObject: T): WidgetResult[T]
 
 }
 
